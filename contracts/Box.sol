a447cd2b449e0a1da721a535c2b1aa6bff343fbd
SSDD <0xSSDD@protonmail.com>
2022-08-09 17:50:37 +0300

    sets up initial repo and basic auth for the box contract

---
+++ contracts/Box.sol
@@ -0,0 +1,24 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+import "./Auth.sol";
+pragma solidity ^0.8.0;
+
+contract Box {
+    uint256 private _value;
+    Auth private _auth;
+
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 value);
+
+    // Stores a new value in the contract
+    function store(uint256 value) public {
+        require(_auth.isAdministrator(msg.sender), "Unauthorised");
+        _value = value;
+        emit ValueChanged(value);
+    }
+
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
