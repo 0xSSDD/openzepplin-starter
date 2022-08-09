568288a018fdbae989847bad962a008e530acefa
SSDD <0xSSDD@protonmail.com>
2022-08-09 22:11:18 +0300

    implements openzepplin ownable

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,18 +1,17 @@
 // contracts/Box.sol
 // SPDX-License-Identifier: MIT
-import "./Auth.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 pragma solidity ^0.8.0;
 
-contract Box {
+contract Box is Ownable {
     uint256 private _value;
-    Auth private _auth;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 value);
 
     // Stores a new value in the contract
-    function store(uint256 value) public {
-        require(_auth.isAdministrator(msg.sender), "Unauthorised");
+    function store(uint256 value) public onlyOwner{
+       
         _value = value;
         emit ValueChanged(value);
     }
